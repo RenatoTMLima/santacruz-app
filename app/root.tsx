d7ae6b4fb67e0813f4886ba3927bfd8260ce4504
Renato Takao Matura de Lima <renato.matura@gmail.com>
2023-02-14 14:26:48 -0300

    Small changes

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -13,6 +13,13 @@ export const meta: MetaFunction = () => ({
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => ([
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  }
+])
 
 export default function App() {
   return (
